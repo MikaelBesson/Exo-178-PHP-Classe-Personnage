97f2c9af98a8b7519ed987bbb7fba913bd513a43
BessonMikael <besson.mikael04@gmail.com>
2021-02-08 13:27:43 +0100

    exo 178

--- classes/Personnage.php
+++ classes/Personnage.php
@@ -4,8 +4,9 @@
 class Personnage {
 
     // La propriété id est privée, je ne peux pas la modifier ou la lire directement en dehors de cette classe
+
     private $id;
-    public $nom;
+    private $nom;
     public $x;
     public $y;
 
@@ -16,11 +17,35 @@ class Personnage {
         $this->y = 0;
     }
 
+
+
     // Méthode permettant de déplacer le personnage vers la droite
     public function walkRight() {
         $this->x += 1;
-
+        echo " ce déplace a droite<br>";
     }
+    // Méthode permettant de déplacer le personnage vers la gauche
+    public function walkLeft(){
+        $this->x -=1;
+        echo "ce déplace a gauche<br>";
+    }
+    // Méthode permettant de déplacer le personnage vers le haut
+    public function walkTop(){
+        $this->y -=1;
+        echo "ce déplace en haut<br>";
+    }
+    // Méthode permettant de déplacer le personnage vers le bas
+    public function walkBottom(){
+        $this->y +=1;
+        echo " ce déplace en bas<br>";
+    }
+
+
+
+
+
+
+
 
     // Setter permettant de définir l'attribut privé id
     public function setId($id) {
@@ -31,4 +56,15 @@ class Personnage {
     public function getId() {
         return $this->id;
     }
+    // Setter permettant de définir l'attribut privé nom
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    // Getter permettant de retourner l'attribut privé nom
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
 } 
--- index.php
+++ index.php
@@ -5,19 +5,26 @@ include "classes/Personnage.php";
 // Creation d'une instance de la classe personnage
 $perso = new personnage();
 
-echo $perso->nom;
+echo $perso->getNom()."<br><br>";
+
+
+$perso->walkRight()."<br>";
+$perso->walkLeft()."<br>";
+$perso->walkTop()."<br>";
+$perso->walkBottom()."<br>";
 
 echo"<br><br>";
 
-
-
 $perso2 = new personnage();
-$perso2->nom = "Robert";
-echo $perso2->nom;
+$perso2->setNom("Robert");
+$perso2->setId("420");
 
 
+echo $perso2->getNom()." : ";
+echo $perso2->getId()."<br><br>";
 
-$perso->walkRight();
-$perso->walkRight();
-$perso->walkRight();
-$perso->walkRight();
+
+$perso2->walkRight()."<br>";
+$perso2->walkLeft()."<br>";
+$perso2->walkTop()."<br>";
+$perso2->walkBottom()."<br>";
